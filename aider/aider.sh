8c96d79b4b8222c325a7bc53dcdc58d0e347e2b2
Marco Angaroni <marco.angaroni@italtel.com>
2025-06-04 11:05:31 +0200

    first version of aider wrapper

---
+++ aider/aider.sh
@@ -0,0 +1,59 @@
+export GEMINI_API_KEY=$(cat $HOME/.config/gemini.token)
+export MISTRAL_API_KEY=$(cat $HOME/.config/mistral.token)
+export HUGGINGFACE_API_KEY=$(cat $HOME/.config/huggingface.token)
+export OPENROUTER_API_KEY=$(cat $HOME/.config/openrouter.token)
+
+OPTS="--no-auto-commits --dark-mode --cache-prompts --watch-files $@"
+
+models=(
+    "gemini/gemini-2.5-flash-preview-05-20"
+    "mistral/mistral-large-latest"
+    "mistral/codestral-latest"
+    "openrouter/qwen/qwen3-235b-a22b:free"
+    "openrouter/deepseek/deepseek-r1:free"
+)
+context_size=(
+    "1000000"
+    "128000"
+    "32000"
+    "40960"
+    "163840"
+)
+if [ -z "$MODEL" ]; then
+    echo "Available Models:"
+    for i in "${!models[@]}"; do
+        echo "$((i+1)). ${models[$i]}"
+    done
+    read -p "Enter model number (default: 1): " choice
+    if [[ -z "$choice" ]]; then
+        MODEL="${models[0]}" # Default to the first model
+        CTX_SIZE=${context_size[0]}
+    elif [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= ${#models[@]} )); then
+        MODEL="${models[$((choice-1))]}"
+        CTX_SIZE=${context_size[$((choice-1))]}
+    else
+        echo "Invalid choice. Using default model: ${models[0]}"
+        MODEL="${models[0]}" # Default if input is invalid
+        CTX_SIZE=${context_size[0]}
+    fi
+else
+    echo "Using model from MODEL environment variable: $MODEL"
+fi
+
+if [ -d .git ]; then
+    USE_GIT=Y
+    REPO_SIZE=$(du --block-size=1 -c $(git ls-files) | tail -1 | cut -f1)
+    # 1 token ≈ 4 characters, 1 character ≈ 1 byte
+    if [ $REPO_SIZE -lt $((CTX_SIZE*4)) ]; then
+        FILES=$(git ls-files)
+        OPTS+=" --map-tokens 0"
+    else
+        FILES=
+    fi
+fi
+if ls CONVENTIONS.md 2> /dev/null; then
+    RFILES="--read CONVENTIONS.md"
+fi
+[ -z "$USE_GIT" ] && OPTS+=" --no-git"
+
+aider --model "$MODEL" $OPTS $FILES $RFILES
